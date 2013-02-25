f5d26f5b78b17b055337803f8000a4a166c25c6b
Vaakapallo <lassi.vapaakallio@cs.helsinki.fi>
2013-02-25 14:58:52 +0200

    Netbeans-pohja

---
+++ Gpong/src/gpong/Gpong.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gpong;
+
+/**
+ *
+ * @author lassi
+ */
+public class Gpong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
