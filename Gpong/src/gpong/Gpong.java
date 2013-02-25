b9878525f7b3a520161042f11d004d3b770dafc8
41407 <jimi.welling@cs.helsinki.fi>
2013-02-25 15:17:16 +0200

    BADONG

--- Gpong/src/gpong/Gpong.java
+++ Gpong/src/gpong/Gpong.java
@@ -14,6 +14,8 @@ public class Gpong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            System.out.println("author 'MUKA' lassi!!!");
+        }
     }
 }
