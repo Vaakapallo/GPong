b0746ec99011fc8d727fdfccde1e9cb85f8d0b51
41407 <jimi.welling@cs.helsinki.fi>
2013-02-25 16:15:38 +0200

    ota ota, et käsi tottuu

--- Gpong/src/ui/Drawer.java
+++ Gpong/src/ui/Drawer.java
@@ -4,10 +4,21 @@
  */
 package ui;
 
+import java.awt.Graphics;
+import java.util.HashSet;
+import logic.Ball;
+import logic.Paddle;
+
 /**
  *
  * @author 41407
  */
 public class Drawer {
+
+    public Drawer() {
+    }
     
+    public void drawPaddle(Paddle p, Graphics g) {
+        g.fillRect(p.getX(), p.getY(), p.getWidth(), p.getHeight());
+    }
 }
